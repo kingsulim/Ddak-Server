a2097cbd336158332e688a7c827e97450377c1c7
YuLim2 <jyulim0120@gmail.com>
2023-10-20 12:22:26 +1100

    INIT: Project Setting

---
+++ src/main/java/com/sulim/ddak/DdakApplication.java
@@ -0,0 +1,13 @@
+package com.sulim.ddak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sulim/ddak/DdakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulim.ddak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
